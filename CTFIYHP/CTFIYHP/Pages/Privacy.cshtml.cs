cfb2a008e43bd3078d5e34ad6f5dbeed90d49141
Aurimasjar <aurimaspetretis@gmail.com>
2023-10-18 10:33:55 +0300

    Original setup

---
+++ CTFIYHP/CTFIYHP/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace CTFIYHP.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("lt-LT"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
